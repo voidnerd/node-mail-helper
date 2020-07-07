a01b6e7f1f5a7719bb0f4d4a7c6607ff543857d1
Ndifreke Friday <ndiecodes@gmail.com>
2020-07-08 00:29:25 +0100

    added ReadME.md

---
+++ README.md
@@ -0,0 +1,78 @@
+# Node-Mail-Helper
+
+Nodemailer Helper For Sending Eamils With Ejs Templating
+
+## Install
+
+```
+npm install node-mail-helper
+```
+
+## Prerequisite
+
+This package requires the following evirmement configs
+
+```
+MAIL_DRIVER=smtp
+MAIL_HOST=smtp.mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=xxxxxxxxxxx
+MAIL_PASSWORD=xxxxxxxxxxx
+MAIL_ENCRYPTION=
+MAIL_FROM=no-reply@ndiecodes.com
+```
+
+N/B: MAIL_ENCRYPTION should be `tls` or `ssl` for secure channels and `blank` for unsecure channels.
+
+## Usage
+
+```
+const Mail = require("node-mail-helper");
+const path = require("path");
+
+const templatePath = path.join(__dirname, "./email/welcome.ejs");
+
+const mail = new Mail();
+ await mail
+      .to(user.email)
+      .template(templatePath) //full path to your ejs file
+      .subject("Welcome Email")
+      .data(mailData)
+      .send();
+```
+
+### The bellow example has all available methods
+
+```
+const Mail = require("node-mail-helper");
+const path = require("path");
+
+const templatePath = path.join(__dirname, "./email/welcome.ejs");
+
+const attactments = [
+        {   // utf-8 string as an attachment
+            filename: 'text1.txt',
+            content: 'hello world!'
+        },
+        {   // binary buffer as an attachment
+            filename: 'text2.txt',
+            content: new Buffer('hello world!','utf-8')
+        },
+];
+
+const mail = new Mail();
+ await mail
+      .from("no-reply@ndiecodes.com")
+      .to(user.email)
+      .cc("janedoe@gmail.com")
+      .bcc("johndoe@gmail.com")
+      .text("String Represenation of Email Body")
+      .template(templatePath)
+      .subject("Welcome Email")
+      .attachments(attachments)
+      .data(mailData)
+      .send();
+```
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,188 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs")
+const { promises: fs } = require('fs');
+
+/** Start Mail Helper */
+
+class Mail {
+
+  constructor() 
+  {
+    this.mailer;
+    this.SUBJECT = null
+    this.FROM = process.env.MAIL_FROM || null;
+    this.DATA = null;
+    this.TO = null;
+    this.CC = null;
+    this.BCC = null;
+    this.ATTACHMENTS = [];
+    this.TEXT = null
+    this.viewEngine;
+    this.option;
+  }
+
+  /**
+   * 
+   * @param {String} subject
+   * @returns {Mail}
+   */
+  subject(subject) {
+    this.SUBJECT = subject;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {String|Array} from
+   * @returns {Mail}
+   */
+  from(from) 
+  {
+    this.FROM = from;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {String|Array} to
+   * @returns {Mail}
+   */
+  to(to) 
+  {
+    this.TO = to;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {String|Array} cc
+   * @returns {Mail}
+   */
+  cc(cc) 
+  {
+    this.CC = cc;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {String|Array} bcc
+   * @returns {Mail}
+   */
+  bcc(bcc) 
+  {
+    this.BCC = bcc;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {Array} attachments
+   * @returns {Mail}
+   */
+  attachments(attachments) 
+  {
+    this.ATTACHMENTS = attachments;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {String|Array} template
+   * @returns {Mail}
+   */
+  template(template) 
+  {
+    this.TEMPLATE = template;
+    return this;
+  }
+
+
+  /**
+   * 
+   * @param {String} text
+   * @returns {Mail}
+   */
+  text(text) 
+  {
+    this.TEXT = text;
+    return this;
+  }
+
+  /**
+   * 
+   * @param {Object} data
+   * @returns {Mail}
+   */
+  data(data) 
+  {
+    this.DATA = data;
+    return this;
+  }
+
+
+  validate()
+  {
+    if(!this.SUBJECT)
+    {
+      throw new Error("Subject Required!")
+    }
+
+    if(!this.FROM)
+    {
+      throw new Error("From Address Required!")
+    }
+
+    if(!this.TO)
+    {
+      throw new Error("TO Address Required!")
+    }
+
+    if(!this.TEMPLATE)
+    {
+      throw new Error("Path to Template File Required!")
+    }
+  }
+
+  /**
+   * 
+   * @returns {Nodemailer Promise}
+   */
+  async send() {
+
+    this.mailer = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure:
+        process.env.MAIL_ENCRYPTION === "ssl" ||
+        process.env.MAIL_ENCRYPTION === "tls",
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+
+    this.validate();
+
+    const htmlStr = await fs.readFile(this.TEMPLATE,
+      'utf8'
+    );
+    const html = ejs.render(htmlStr, this.DATA);
+
+    const mailOptions = {
+      from: `"${process.env.APP_NAME}" <${this.FROM}>`,
+      to: this.TO,
+      subject: this.SUBJECT,
+      cc: this.CC,
+      bcc: this.BCC,
+      attachments: this.ATTACHMENTS,
+      text: this.TEXT,
+      html: html,
+    };
+    return this.mailer.sendMail(mailOptions);
+  }
+}
+
+/** End Mail Helper */
+
+module.exports = Mail;
